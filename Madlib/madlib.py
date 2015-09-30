14bc84d8ff96b92762ca6fcd0bbf4be12b606d89
Jana Nash-Siegle <jsiegle@jlsdesk.com>
2015-09-30 18:06:24 -0500

    Added Intro / Title ..and
    with exception of the array, I have tried to have no hard coded values.
     I believe I have been successful in utilizing user input for every
    value needed.  I have two places in the story where user input
    influences the direction of the story, user input determines names of
    all the characters.  This has been fun.  It’s a dorky story, it may not
    be perfect grammatically, but I was more worried about the code and
    must move on if I am to get the other two assignments done by next
    Wednesday.  Awesome assigment

--- Madlib/madlib.py
+++ Madlib/madlib.py
@@ -27,9 +27,13 @@ friend = raw_input("What is your favorite name, " +name +"? ") #lets add a name 
 vehicle = raw_input(name+", name any type of vehicle, land, air, or water please? ")
 #print vehicle   #what vehicle will the verb utilize in the story?
 
+best_friend = raw_input ("Please choose a number between 1-5, thank you " + name + ".  ")
+
 players = ["Mike", "John", "Chris", "Larry", "Moe"]  #and some friends or students or pets, it's a name pool
 players.append(friend)
 #print players
+z = int(best_friend)
+#print players[z]  works 
 
 cookies = raw_input(name+", Pick a number between 1-10? ") #need it to determine bridge access
 
@@ -55,9 +59,10 @@ print "++++++++++++===================================++++++++++++++++++"
 print "++++++++++++===================================++++++++++++++++++"
 print "A Trip To the Store" # title of our story
 
+print "We begin our story at the home of  " + name + " and family.  It is a normal day in this quiet little town, but alas, the cupboard seems to be bare.  Let us listen in and watch."
 print "     " + name+", would you " + verb + " to the store and pick up " + str(calcStone(stone_weight,weight)) + " pounds of flour so I can make " + dessert + " for " + meal + "?"
 print name + " grabs a/some " + color + " " + clothing + " to slip on while picking up the leash so the family " + animal + " named" + " " + friend + " could go along for the journey."
-print "Hopping into the " + vehicle + " " + name + " calls " + players[3] + " " + '"Meet me at the bridge" as the '  + str(fruit[u_fruit]) + " " + vehicle + " speeds off in that direction." 
+print "Hopping into the " + vehicle + " " + name + " calls " + players[z] + " her best friend 'Meet me at the bridge' as the "  + str(fruit[u_fruit]) + " " + vehicle + " speeds off in that direction." 
 print "Arriving at the bridge the troll guard demands the toll be paid."
 #I need if else results here to determine course of story
 
@@ -67,11 +72,11 @@ else:
 	if int(cookies) >= 5: #upon user entry it's a str and it needs to be an int
 		print "'I will gladly pay your toll, kind Sir!'" + name + " replies and is allowed to pass."
 
-print "     As the bridge becomes a small feature in the distance " + name + " finally finds " + players[3] + " who appears to be holding a small stone in his hand.  'What is that you have?,' " + name + " asks while reaching for the small object.  Hand jerking away a loud, if childish reply can be heard 'Do not touch! It is mine!'" + players[3] + " shouts out.  A collective sigh can be heard from all the other players, " + ', '.join(players) + " at this childish display."
+print "     As the bridge becomes a small feature in the distance " + name + " finally finds " + players[z] + " who appears to be holding a small stone in his hand.  'What is that you have?,' " + name + " asks while reaching for the small object.  Hand jerking away a loud, if childish reply can be heard 'Do not touch! It is mine!'" + players[z] + " shouts out.  A collective sigh can be heard from all the other players, " + ', '.join(players) + " at this childish display."
 from random import randint
 #print(randint(0,int(ran_num))) #generate a random number (for test use only)
 if int(ran_num) < 50:  #let's use the random generated number to decide
-	print "With slumped shoulders and a sly grin, Chris holds out the small bauble for all to see." #the fate of the story
+	print "With slumped shoulders and a sly grin, " + players[z] + " holds out the small bauble for all to see." #the fate of the story
 else:
 	if int(ran_num) >= 50:
 		print " With a huff, " + name + " replies, 'Fine then, we need to get going anyway!'"
@@ -80,11 +85,11 @@ else:
 questions = ['favorite dessert', 'mom making then today?', 'favorite food'] #question nouns
 answers = ['Strawberry Cake', ' hmmm, I honestly do not know', 'macaroni and cheese'] #answers 
 
-print "After a period of time, they arrive at the store and pick out the flour that was needed.  Larry all of a sudden gets all excited thinking about the upcoming treats and inundates " + name + " with questions, non-stop."
+print "After a period of time, they arrive at the store and pick out the flour that was needed." + players[z] + " suddenly gets all excited thinking about the upcoming treats and inundates " + name + " with questions, non-stop."
 for q, a in zip(questions, answers): #for statement zip function	
 	print "What is your {0}? It is {1}. ".format(q, a) #formatting printing q a  list out questions
 
-print "With a hand held high " + name + " interrupts " + players[3] + " hold on there big fella, why don't we just wait until we get home, give mom time to cook us up some yummy vittles and we can discuss this like the good cowpokes we are?, " + name + " replies with a laugh and fake accent.  With that the two head home and are treated to a wonderful " + meal + "."
+print "With a hand held high " + name + " interrupts " + players[z] + " hold on there big fella, why don't we just wait until we get home, give mom time to cook us up some yummy vittles and we can discuss this like the good cowpokes we are?, " + name + " replies with a laugh and fake accent.  With that the two head home and are treated to a wonderful " + meal + "."
 
 print "++========== THE END =========++"
 
--- Madlib/test.py
+++ Madlib/test.py
@@ -1,16 +1,12 @@
 #used as a playground to test/debug bits of the program at a time
-
 name = raw_input ("What is your name?  ")  #get player name for use within game
-ran_num = raw_input("Choose a number between 1 and 100: ")#this number will be used to determine the end of the range a random number is generated from
+friend = raw_input("What is your favorite name, " +name +"? ") #lets add a name for the family pet
+best_friend = raw_input ("Please choose a number between 1-5, thank you " + name + ".  ")
 
-from random import randint
-print(randint(0,int(ran_num))) #generate a random number
-if int(ran_num) < 50:  #let's use the random generated number to decide
-	print "With slumped shoulders and a sly grin, Chris holds out the small bauble for all to see." #the fate of the story
-else:
-	if int(ran_num) >= 50:
-		print " With a huff, " + name + " replies, 'Fine then, we need to get going anyway!'"
+players = ["Mike", "John", "Chris", "Larry", "Moe"]  #and some friends or students or pets, it's a name pool
 
+z = int(best_friend)
+print players[z]
 
 
 
