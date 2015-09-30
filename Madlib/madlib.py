9ca1fdd6da5a323321d0f43210dd1dceea1836a3
Jana Nash-Siegle <jsiegle@jlsdesk.com>
2015-09-29 21:09:38 -0500

    game updates
    More responses,trying to get pound to stone conversion working.  need
    my loop then on to story

--- Madlib/madlib.py
+++ Madlib/madlib.py
@@ -1,20 +1,44 @@
-print "Welcome to Madlibs!  A fantasy world created by your words and imagination"
+print "Welcome to Madlibs!  A fantasy world created by your words and imagination" #Welcome message
 
-name = response = raw_input ("What is your name?  ")
+name = response = raw_input ("What is your name?  ")  #get player name for use within game
+'''
+response = raw_input(name+", please pick a verb: ") #grab a verb to use in story
 
-response = raw_input(name+", please pick a verb: ")
+response = raw_input("Please pick a noun: ")  #grab a noun
 
-response = raw_input("Please pick a noun: ")
+response = raw_input("Thank you " + name+", now would you please pick an adjective: ") #and an adjective
 
-response = raw_input("Thank you " + name+", now would you please pick an adjective: ")
+response = raw_input(name+", what is your favorite color: ") #what's a story without a color
 
-response = raw_input(name+", what is your favorite color: ")
+response = raw_input("Name an animal that lives on a farm. ") #and the animal
 
-response = raw_input("Name an animal that lives on a farm. ")
+response = raw_input(name+", What article of clothing are you wearing right now: ") #not getting too personal are we?  nawwww
 
-response = raw_input(name+", What article of clothing are you wearing right now: ")
+ran_num = response = raw_input("Choose a number between 1 and 100: ")#this number will be used to determine the end of the range a random number is generated from
 
-ran_num = response = raw_input("Choose a number between 1 and 100: ")
-
+friend = response = raw_input("What is your favorite name, " +name +"? ")
 from random import randint
-print(randint(0,int(ran_num)))
+print(randint(0,int(ran_num))) #generate a random number
+
+players = ["Mike", "John", "Chris", "Larry", "Moe"]
+players.append(friend)
+print players
+
+
+
+response = raw_input(name+", How many cookies in a baker's dozen? ")
+
+even = response = raw_input("Quick!  What is the first even number you can think of? ")
+
+fruit = dict()
+fruit = {"apple":"red", "lime":"green", "banana":"yellow"}
+print fruit ["apple"]
+'''
+weight = response = raw_input(name+", How much do you weigh? ")
+
+stone_weight = 0.0714285714
+def calcStone(weight, stone_weight):
+	calcStone(weight * stone_weight)
+	return calcStone
+
+	print calcStone
