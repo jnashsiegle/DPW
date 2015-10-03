503ffaa2e10aaf5e14113678ca3c1716bf543e2a
Jana Nash-Siegle <jsiegle@jlsdesk.com>
2015-10-02 23:08:52 -0500

    Form - Layout Complete
    Sending info to new page not done, running into page attribute errors.
    As is now, no errors.  Start fresh in morning.
    Styling not complete.

--- event-form/main.py
+++ event-form/main.py
@@ -10,7 +10,7 @@ from pages import Page #from pages.py import the page class
 class MainHandler(webapp2.RequestHandler):
     def get(self):
         p = Page()
-        p.body = "Miss Piggy likes Kermit de Frog" #this will replace the body text below in self.body
+        #p.body = "Miss Piggy likes Kermit de Frog" #this will replace the body text below in self.body
         #print p.print_out()#this will print out in the google engine console
         self.response.write(p.print_out())# this will print out in browser
 
