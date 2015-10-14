6b1c066ed12bdb8d440b8dece27d0a1f132ea8e0
Jana Nash-Siegle <jsiegle@jlsdesk.com>
2015-10-14 14:49:06 -0500

    two classes defined, ResultsPage and Form

--- nash_jana_library/main.py
+++ nash_jana_library/main.py
@@ -1,11 +1,12 @@
 
 import webapp2
 #from library import 
-from pages import ResultsPage
+from pages import Form, ResultsPage
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
     	rp = ResultsPage()
+    	f = Form()
     	#lib =  #instance for the FavoriteMovies
         #movie title  (this is part of our assignment, making it user input)
         #year movie was made(this is part of our assignment)(this is our assignment)
@@ -14,20 +15,13 @@ class MainHandler(webapp2.RequestHandler):
         #page for class (if/else for 2 different page views for assignment)
 
         '''
-        ***** example data
-        md1 = MovieData()
-        md1.title = "The Princess Bride"
-        md1.year = 1989  #actually calling a function 
-        md1.director = "Rob Reiner"
-        lib.add_movie(md1) #causes title to print out when print m.title is added to method def addMovie
-        '''
         
-        '''
         ***** call and run function examples
         lib.calc_time_span()    #adds this to run but we need to add it to the printing out list below
         p.body = lib.compile_list() + lib.calc_time_span() #adds the compile list to the body tag of the html in the page.py
-        self.response.write(p.print_out()) #sends the info out to browser as a big string
         '''
+        self.response.write(rp.print_out()) #sends the info out to browser as a big string
+        
 
 
 
--- nash_jana_library/pages.py
+++ nash_jana_library/pages.py
@@ -1,3 +1,77 @@
+class Form(object):
+	def __init__(self):
+		self.head = """   				
+<!DOCTYPE HTML>							
+<html>
+	<head>
+		<title>Event Planning Survey</title>
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+	</head>
+	<body>"""
+
+		self.header = '''
+		<header><h1>Acme Web Workshop Event Planning Survey</h1>
+		<p><a href = "http://www.bing.com">Exit Survey</a></p>
+		</header>'''
+		self.body = '''<article id = "wrapper">
+									
+						<h3>Thank you for signing up for our next workshop.</h3> <p> Would you take a moment to fill out this survey so we may learn how to better serve you?</p>
+					
+					
+					<form method = "GET">
+						<div id = "personalInfo">
+							<label>Name: </label><input type = "text" placeholder = "Enter Your Name" name = "attendee" />
+							<label>Email: </label><input type = "email" placeholder = "yourname@domain.com" name = "email" />
+						</div>
+						<div id = "workshops">
+							<label>Which workshop will you be attending?</label>
+								<select name = "workshops">
+									<option value = "">Click here to see options</option>
+									<option value = "css">CSS</option>
+									<option value = "html">HTML</option>
+									<option value = "js">JavaScript</option>
+									<option value = "python">Python</option>
+								</select>
+							
+						</div>
+
+						<div id = "registration">
+							<label>How did you hear about this event?</label><input type = "text" placeholder = "online/word of mouth" name = "hear" required />
+							<p>How easy was the registration process for this event?</p>
+							<label>Very Easy</label><input type = "radio" name = "rp" value = "very easy" checked/>
+							<label>Easy</label><input type = "radio" name = "rp" value = "easy"/>
+							<label>Slightly Confusing</label><input type = "radio" name = "rp" value = "slightly confusing"/>
+							<label>Hard</label><input type = "radio" name = "rp" value = "hard"/>
+							<label>Very Hard</label><input type = "radio" name = "rp" value = "very hard" />
+							<p></p>
+							<label>Comments about the registration process: </label><input type = "text" placeholder = "Make comments here" name = "rpComments" value = "none"/>
+						</div>
+						<div id = "diet">
+							<p>Do you have any dietary restrictions?
+							<label>Yes</label><input type = "radio" name = "diet" value = "do"/>
+							<label>No</label><input type = "radio" name = "diet" value = "don't" checked/>
+							<p>if yes, please enter details: </p>
+							<label>Details: </label><input type = "text" placeholder = "Restrictions" name = "dietDetails" value = "none"/></p>
+						</div>
+						<div id = "misc">
+							<label>Who will be paying for you to attend this event?</label><input type = "text" placeholder = "company" name = "payment" value = "none">
+							<p>What is the preferred method of contacting you in regards to this event?</p>
+							<label>E-Mail</label><input type = "radio" name = "communication" value = "email" checked/>
+							<label>Phone</label><input type = "radio" name = "communication"  value = "phone"/>
+							<label>Snail Mail</label><input type = "radio" name = "communication" value = "snail mail" />
+							<p>What is your interest in these workshops?</p>
+							<label>Career</label><input type = "checkbox" name = "purpose" value = "career" checked />
+							<label>Passion</label><input type = "checkbox" name = "purpose" value = "passion" />
+							<label>Hobby</label><input type = "checkbox" name = "purpose" value = "hobby" />
+						</div>						
+						<p><input type = "submit" value = "Submit" /></p>
+		'''
+		self.close = '''
+					</form>
+				</article>
+	</body>
+</html>'''
+
 class ResultsPage(object):
 	def __init__(self):
 		self.__title = "Welcome!"
