d9d1d164a4ed49a84e3472eac80d03d71183f3d9
Jana Nash-Siegle <jsiegle@jlsdesk.com>
2015-10-14 22:16:56 -0500

    starting over completely

--- nash_jana_library/library.py
+++
@@ -1,9 +0,0 @@
-class TranInfo(object):
-	def __init__(self):
-		#attributes of transaction entries
-		self.payee = ''
-		self.amount = 0
-		self.date = ''
-		self.category = ''
-		self.budget = ''
-		
