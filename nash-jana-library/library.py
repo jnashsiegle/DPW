47a8e31757e48fc63046349452221bb3e66caffa
Jana Nash-Siegle <jsiegle@jlsdesk.com>
2015-10-15 19:53:45 -0500

    backing up trying some different code in library....to get jump started on correct getting/setting

--- nash-jana-library/library.py
+++ nash-jana-library/library.py
@@ -23,16 +23,14 @@ class BudgetBreak(object):
 	def mon_income(self):
 	    return self.__mon_income	
 
-	'''def discretion(self):
-		self.discretion = self.__mon_income - self.__mon_exp
-	    return self.discretion'''
+	
 
 	def mortgage(self):
-		self.mortgage = self.__mon_income * .25
+		self.mortgage = (self.__mon_income * .25)
 		return self.mortgage
 
 	def annual_income(self):
-		self.annual_income = self.__mon_income * 12
+		self.annual_income = (self.__mon_income * 12)
 		return self.annual_income
 		
 
